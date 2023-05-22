d0c55c599984bec23f7f8413852702d0dc942bd7
Therawaji <82218221+Therawaji@users.noreply.github.com>
2023-05-23 01:25:37 +1000

    1

---
+++ docker_file_front/front_end_file/src/components/Cob.vue
@@ -0,0 +1,36 @@
+<template>
+    <div align = 'center'>
+        <div id="map" style="height: 100px"></div>
+        <img src="@/components/testmap.png" style="height:350px; width:350px; object-fit:cover;">
+        <h1>This is 3rd View</h1>
+        <ul>{{string}}</ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ThirdView',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+</style>
+
