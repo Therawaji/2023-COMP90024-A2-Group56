f3d1796567dd678c16fa409b8a5a14857faec50b
bypau <127822044+bypau@users.noreply.github.com>
2023-05-26 03:44:59 +1000

    Update life_data_process.R

--- Sudo/sudo data pre-processing/life_data_process.R
+++ Sudo/sudo data pre-processing/life_data_process.R
@@ -9,8 +9,4 @@
 df <- read.csv("sudo original data/abs_life_tables_sa4_2010_2019-7439425676767672303.csv")
 code_name <- read.csv("sudo original data/sa4 code.csv")
 df <- merge(df, code_name, by.x="sa4_code16",by.y ="SA4_CODE21")
-library(rgdal)
-myspdf <- readOGR("E:/chrome download/SA4_2021_AUST_SHP_GDA2020/SA4_2021_AUST_GDA2020.shp")
-mynewspdf <- merge(myspdf, df, by.y="sa4_code16",by.x='SA4_CODE21')
 write.csv(df, "E:/chrome download/Life_Tables/life.csv")
-writeOGR(mynewspdf, dsn = 'E:/chrome download/', layer = 'poly', driver = "ESRI Shapefile")
